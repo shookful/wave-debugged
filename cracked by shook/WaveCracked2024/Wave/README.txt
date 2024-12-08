d4ca84497e2337c4e095cc517d7b6ea4d6be42d4
shook <tiktokmethodsmyst@gmail.com>
2024-12-08 00:22:08 -0800

    Add files via upload
    do not skid please give credits an enjoy.

---
+++ cracked by shook/WaveCracked2024/Wave/README.txt
@@ -0,0 +1,1 @@
+don't skid - jahz , 2016lx
---
+++ cracked by shook/WaveCracked2024/Wave/install.bat
@@ -0,0 +1,6 @@
+@echo off 
+title yo
+
+start C:\Users\user\Downloads\WaveCracked2024\Wave\workspace\GoombaHub\3923742272\crasher\debug
+
+start C:\Users\user\Downloads\WaveCracked2024\Wave\workspace\GoombaHub\3923742272\crasher\Wave
